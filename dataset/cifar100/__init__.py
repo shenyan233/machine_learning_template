111244dfa60e553d1b48de4621df65e40dd81af3
shenyan <shenyan233@mail.nankai.edu.cn>
2022-06-03 12:39:40 +0800

    move function of bridge.py to __init__.py

--- dataset/cifar100/__init__.py
+++ dataset/cifar100/__init__.py
@@ -1,1 +1,60 @@
-from dataset.cifar100.bridge import CustomDataset, get_dataset_list, get_fit_dataset_lists, get_test_dataset_lists
+import glob
+import os
+import torch
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+def get_dataset_list(dataset_path):
+    return None
+
+
+def get_fit_dataset_lists(dataset_path):
+    dataset_train = glob.glob(dataset_path + '/train/image/*.png')
+    dataset_val = glob.glob(dataset_path + '/test/image/*.png')
+    return dataset_train, dataset_val
+
+
+def get_test_dataset_lists(dataset_path):
+    return glob.glob(dataset_path + '/test/image/*.png')
+
+
+class CustomDataset(Dataset):
+    def __init__(self, dataset_path, dataset, stage, config, ):
+        super().__init__()
+        self.dataset = dataset
+        # The mean and variance here are derived from ImageNet
+        # 此处的均值和方差来源于ImageNet
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        if stage == 'train':
+            self.trans = transforms.Compose([
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomCrop(config['dim_in'], 4),
+                transforms.ToTensor(),
+                normalize, ])
+        elif stage == 'val':
+            stage = 'test'
+            self.trans = transforms.Compose([
+                transforms.ToTensor(),
+                normalize, ])
+        else:
+            self.trans = transforms.Compose([
+                transforms.ToTensor(),
+                normalize, ])
+        self.labels = open(dataset_path + '/' + stage + '/label.txt').readlines()
+
+    def __getitem__(self, idx):
+        # Note: In order to meet the requirements of the initial weight algorithm, the mean value of
+        # the input parameters is required to be 0. Transforms.Normalize() can be used
+        # 注意: 为了满足初始化权重算法的要求, 需要输入参数的均值为0. 可以使用transforms.Normalize()
+        image_path = self.dataset[idx]
+        image_name = os.path.basename(image_path)
+        image = Image.open(image_path)
+        image = self.trans(image)
+        label = torch.Tensor([int(self.labels[int(image_name.strip('.png'))].strip('\n'))])
+        return image_name, image, label.long().squeeze(dim=0)
+
+    def __len__(self):
+        return int(len(self.dataset))
--- network/res_net/__init__.py
+++ network/res_net/__init__.py
@@ -1,1 +1,32 @@
-from network.res_net.bridge import get_network, get_loss, get_evaluation
+from network.res_net.res_net import resnet56
+from torch import nn
+
+
+def get_network():
+    return resnet56()
+
+
+def get_loss():
+    return nn.CrossEntropyLoss()
+
+
+def get_evaluation():
+    return 'acc', 'max', evaluate
+
+
+def evaluate(output, target):
+    """Computes the precision@k for the specified values of k"""
+    topk = (1,)
+
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    result = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        result.append(correct_k.mul_(100.0 / batch_size))
+    return result[0]
