a9e4b3ef31bdc10c51349a6537204f2f123fe004
shenyan <shenyan233@mail.nankai.edu.cn>
2023-08-08 02:17:39 +0800

    Obtain batch size for validation and testing processes through calculations

--- data_module.py
+++ data_module.py
@@ -80,9 +80,39 @@ class DataModule(pl.LightningDataModule):
                           num_workers=self.num_workers, pin_memory=self.pin_memory)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.config['batch_size'], shuffle=False,
+        """
+        Since the loss method used by PL to calculate and verify epoch calculated the mean value of the loss of
+        each batch, the mean value would also be calculated when multiple samples were calculated within each batch,
+        which led to the mean value calculation twice (this problem would also occur in official CELOSS). If the sizes
+        of two batches are different at this time, the weight of each loss is not equal, which leads to the error of
+        obtaining Loss. This situation often occurs, because the amount of data cannot be divided exactly by size, so
+        basically the size of the last batch is different from the size of the previous batch.
+        In order to ensure the accuracy of verification set calculation of Loss, batch size of verification set is
+        redefined in this method. In addition, the backpropagation of the training set is not affected, but
+        the loss record of the training set is. However, since batCH_size has a great influence on the training results,
+        the impact of loss record is ignored and the training set Batch size is not redefined.
+        由于pl计算验证epoch的loss的方法为每个batch的loss求均值, 而每个batch内计算多个样本时同样会求均值,
+        这导致了两次求均值(官方的celoss也会出现该问题). 如果这时存在两个batch的size不同,则会导致每个loss
+        的权重不相等, 导致求loss的错误. 这种情况常常出现, 因为数据量不能整除size, 所以基本上最后一个batch
+        的size与前面的batch的size不同.
+        为了保证验证集计算loss的准确性, 该方法中对于验证集的batch size进行了重新定义.
+        此外, 训练集的反向传播不受影响, 但训练集的loss记录会受影响. 然而, 由于batch_size对训练结果具有较大
+        的影响, 因此, 忽略loss记录的影响, 不对训练集batch size进行重新定义.
+        """
+        val_batch_size = 1
+        for num in range(self.config['batch_size']):
+            if len(self.val_dataset) % (self.config['batch_size'] - num) == 0:
+                val_batch_size = self.config['batch_size'] - num
+                break
+        return DataLoader(self.val_dataset, batch_size=val_batch_size, shuffle=False,
                           num_workers=self.num_workers, pin_memory=self.pin_memory)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=1, shuffle=False, num_workers=self.num_workers,
+        test_batch_size = 1
+        for num in range(self.config['batch_size']):
+            if len(self.val_dataset) % (self.config['batch_size'] - num) == 0:
+                test_batch_size = self.config['batch_size'] - num
+                break
+        return DataLoader(self.test_dataset, batch_size=test_batch_size, shuffle=False,
+                          num_workers=self.num_workers,
                           pin_memory=self.pin_memory)
